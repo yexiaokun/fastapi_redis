5b29e83287ed4fb0c7ee11ef9de76e695d3c3966
Autumn <1667877328@qq.com>
2025-11-26 17:56:13 +0800

    未完成

--- app/database.py
+++ app/database.py
@@ -1,11 +1,15 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
+import redis.asyncio as aioredis
 
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
 DB_NAME = os.getenv("DB_NAME")
+REDIS_URL = os.getenv("REDIS_URL")
 
-client = AsyncIOMotorClient(MONGO_URL)
-db = client[DB_NAME]
+mongo_client = AsyncIOMotorClient(MONGO_URL)
+db = mongo_client[DB_NAME]
+
+redis_client = aioredis.from_url(REDIS_URL)
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import users, posts
+from app.database import db, redis_client
 
 
 app = FastAPI(title="FastAPI ~ Redis Blog")
@@ -12,3 +13,17 @@ app.include_router(posts.router)
 @app.get("/")
 def read_root():
     return {"message": "欢迎来到我的个人博客！"}
+
+@app.get("/healthcheck")
+async def healthcheck():
+    try:
+        collections = await db.list_collection_names()
+
+        pong = await redis_client.ping()
+        return {
+            "status": "ok",
+            "mongo_collections": collections,
+            "redis_connected": pong
+        }
+    except Exception as e:
+        return {"status": "error", "detail": str(e)}
